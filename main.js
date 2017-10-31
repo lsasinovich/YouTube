9cbb823c2d3e97142a44e5a55fab278d169e4d30
Lizaveta <liza_sasinovich@mail.ru>
2017-11-01 01:52:00 +0200

    Implement carousel for big and medium screen

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var carousel = document.getElementById('carousel');
+/*var carousel = document.getElementById('carousel');
   
 
 function carouselHide() {
@@ -35,8 +35,8 @@ function setSlide(slide) {
 
 
 if (carousel) {
-    var slides = carousel.querySelectorAll('.result-item');
-    var indicators = carousel.querySelectorAll('.indicator');
+    var slides = carousel.getElementsByClassName('result-item');
+    var indicators = carousel.getElementsByClassName('indicator');
     var indicator = indicators[0];
 
     if (window.innerWidth >800) {
@@ -69,7 +69,7 @@ function resize() {
         j=3;
         var c = indicators.length;
         for (var i=0; i<c/2; i++) {
-            document.querySelector(".indicators").removeChild(document.querySelector(".indicators").lastChild);
+            document.querySelector(".indicators").removeChild(document.querySelector(".indicators").lastElementChild);
         }
     } else if (window.innerWidth <= 800 && window.innerWidth > 480 && count != 2) {
         count = 2;
@@ -107,3 +107,130 @@ function resize() {
     }
 }
 
+*/
+
+
+function carouselHide() {
+      for (var i = 0; i<slides.length; i++) {
+            slides[i].setAttribute('data-state', '');
+            slides[i].style.display = 'none';
+        }
+
+        for (var i = 0; i < indicators.length; i++) {
+            indicators[i].setAttribute('data-state', '');            
+        }
+}
+
+function carouselShow(num) {
+        indicators[num].setAttribute('data-state', 'active');
+        indicators[num].checked = true;
+
+        for (var i = 0; i<itemsCount; i++) {
+            slides[num*itemsCount + i].setAttribute('data-state', 'active');
+            slides[num*itemsCount+i].style.display = 'inline-block';
+        }
+}
+
+function setSlide(slide) {
+    return function() {               
+        
+        carouselHide();
+        
+        carouselShow(slide);
+
+    };
+}
+
+function addIndicator(start, count, parent) {
+    for (let i = start; i < count; i++) {
+        let node = document.createElement("input");
+
+        node.setAttribute("class", "indicator");
+        node.setAttribute("name", "indicator");
+        node.setAttribute("type", "radio");
+        node.setAttribute("data-slide", `${i}`);
+        node.addEventListener("click", setSlide(i));
+
+        parent.appendChild(node);
+    }
+}
+
+function resize() {
+    if (window.innerWidth < 800 && itemsCount == 4) {
+        for (let i = 0; i < 3; i++) {
+            let node = document.createElement("input");
+
+            node.setAttribute("class", "indicator");
+            node.setAttribute("name", "indicator");
+            node.setAttribute("type", "radio");
+            node.setAttribute("data-slide", `${indicatorsCount + i}`);
+            node.addEventListener("click", setSlide(indicatorsCount + i));
+
+            indicatorsParent.appendChild(node);
+        }
+        itemsCount = 2;
+        indicatorsCount = Math.floor(slides.length/itemsCount);
+
+    for (let i = 0; i < indicatorsCount; i++) {
+        if( indicators[i].checked ==  true) {
+            console.log('here');
+            setSlide(i)();
+            break;
+        }
+    }
+    }
+
+    if (window.innerWidth > 800 && itemsCount == 2) {
+        
+        for (let i = 0; i < 3; i++) {
+            indicatorsParent.removeChild(indicatorsParent.lastElementChild);
+        }
+        itemsCount = 4;
+        indicatorsCount = Math.floor(slides.length/itemsCount);
+
+    for (let i = 0; i < indicatorsCount; i++) {
+        if( indicators[i].checked ==  true) {
+            console.log('here')
+            setSlide(i)();
+            break;
+        }
+    }
+    }
+}
+
+var carousel = document.getElementById('carousel');
+var slides = carousel.getElementsByClassName('result-item');
+var indicators = carousel.getElementsByClassName('indicator');
+var indicatorsParent = carousel.querySelector('.indicators');
+window.onresize = resize;
+
+var itemsCount;
+var indicatorsCount;
+
+if(window.innerWidth > 800) {
+    itemsCount = 4; 
+} else if (window.innerWidth > 480) {
+    itemsCount = 2; 
+} else {
+    itemsCount = 1;
+}
+
+indicatorsCount = Math.floor(slides.length/itemsCount);
+
+for (let i = 0; i < indicatorsCount; i++) {
+    let node = document.createElement("input");
+
+    node.setAttribute("class", "indicator");
+    node.setAttribute("name", "indicator");
+    node.setAttribute("type", "radio");
+    node.setAttribute("data-slide", `${i}`);
+    node.addEventListener("click", setSlide(i));
+
+    indicatorsParent.appendChild(node);
+}
+
+indicators[0].checked = "true";
+
+for (let i = 0; i < itemsCount; i++) {
+    slides[i].setAttribute('data-state', 'active');
+}
