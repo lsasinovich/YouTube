8468619ff29c456ea49399ec4cf26dfb408dd3c3
lsasinovich <liza_sasinovich@mail.ru>
2017-11-01 11:00:33 +0300

    Initial carousele for 12 items

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-/*var carousel = document.getElementById('carousel');
-  
-
+/* Hiding carousele (set attribetes display:none and data-state: "")*/
 function carouselHide() {
       for (var i = 0; i<slides.length; i++) {
             slides[i].setAttribute('data-state', '');
@@ -8,116 +6,7 @@ function carouselHide() {
         }
 
         for (var i = 0; i < indicators.length; i++) {
-            indicators[i].setAttribute('data-state', '');            
-        }
-}
-
-function carouselShow(num) {
-        indicators[num].setAttribute('data-state', 'active');
-        indicators[num].checked = true;
-
-        for (var i = 0; i<count; i++) {
-            slides[num*count + i].setAttribute('data-state', 'active');
-            slides[num*count+i].style.display = 'inline-block';
-        }
-}
-
-function setSlide(slide) {
-    return function() {               
-        
-        carouselHide();
-        
-        carouselShow(slide);
-
-    };
-}
-
-
-
-if (carousel) {
-    var slides = carousel.getElementsByClassName('result-item');
-    var indicators = carousel.getElementsByClassName('indicator');
-    var indicator = indicators[0];
-
-    if (window.innerWidth >800) {
-        var count = 4;
-        var j=3;
-    } else if (window.innerWidth > 480) {
-        var count = 2;
-        var j=1;
-    }
-    else {
-        var count = 1;
-        var j=0;
-    }
-    while (j !== -1) {
-        slides[j].setAttribute('data-state', 'active');
-        j--;
-    }
-
-    for (var i = 0; i < indicators.length; i++) {
-        indicators[i].addEventListener("click", setSlide(i));
-    }
-};
-
-window.onresize = resize;
-
-function resize() {
-    
-    if (window.innerWidth > 800 && count != 4) {
-        count = 4;
-        j=3;
-        var c = indicators.length;
-        for (var i=0; i<c/2; i++) {
-            document.querySelector(".indicators").removeChild(document.querySelector(".indicators").lastElementChild);
-        }
-    } else if (window.innerWidth <= 800 && window.innerWidth > 480 && count != 2) {
-        count = 2;
-        j=1;
-        var c = indicators.length;
-        for (var i=0; i<c; i++) {
-            var node = document.createElement('input');
-            node.setAttribute('class', 'indicator');
-            node.setAttribute('name', 'indicator');
-            node.setAttribute('type', 'radio');
-
-            document.querySelector(".indicators").append(node);
-
-        }
-    }
-    else if (window.innerWidth <= 480){
-        count = 1;
-        j=0;
-    }
-
-    indicators = carousel.querySelectorAll('.indicator');
-     for (var i = 0; i<slides.length; i++) {
-            slides[i].setAttribute('data-state', '');
-            slides[i].style.display = 'none';
-        }
-    for (var i=0; i<indicators.length; i++) {
-        if(indicators[i].getAttribute("data-state") == "active") {
-
-            carouselShow(i); 
-        }
-    }
-    
-    for (var i = 0; i < indicators.length; i++) {
-        indicators[i].addEventListener("click", setSlide(i));
-    }
-}
-
-*/
-
-
-function carouselHide() {
-      for (var i = 0; i<slides.length; i++) {
-            slides[i].setAttribute('data-state', '');
-            slides[i].style.display = 'none';
-        }
-
-        for (var i = 0; i < indicators.length; i++) {
-            indicators[i].setAttribute('data-state', '');            
+            indicators[i].setAttribute('data-state', '');
         }
 }
 
@@ -132,10 +21,10 @@ function carouselShow(num) {
 }
 
 function setSlide(slide) {
-    return function() {               
-        
+    return function() {
+
         carouselHide();
-        
+
         carouselShow(slide);
 
     };
@@ -156,7 +45,7 @@ function addIndicator(start, count, parent) {
 }
 
 function resize() {
-    if (window.innerWidth < 800 && itemsCount == 4) {
+    if (window.innerWidth <= 800 && itemsCount == 4) {
         for (let i = 0; i < 3; i++) {
             let node = document.createElement("input");
 
@@ -169,32 +58,81 @@ function resize() {
             indicatorsParent.appendChild(node);
         }
         itemsCount = 2;
-        indicatorsCount = Math.floor(slides.length/itemsCount);
+        indicatorsCount = Math.ceil(slides.length/itemsCount);
 
-    for (let i = 0; i < indicatorsCount; i++) {
-        if( indicators[i].checked ==  true) {
-            console.log('here');
-            setSlide(i)();
-            break;
+        for (let i = 0; i < indicatorsCount; i++) {
+            if( indicators[i].checked ==  true) {
+                console.log('here');
+                setSlide(i*2)();
+                break;
+            }
         }
-    }
     }
 
     if (window.innerWidth > 800 && itemsCount == 2) {
-        
+
         for (let i = 0; i < 3; i++) {
+            if(indicatorsParent.lastElementChild.checked == true) {
+              console.log("ok");
+              indicatorsParent.lastElementChild.previousElementSibling.checked = true;
+            }
             indicatorsParent.removeChild(indicatorsParent.lastElementChild);
         }
         itemsCount = 4;
         indicatorsCount = Math.floor(slides.length/itemsCount);
 
-    for (let i = 0; i < indicatorsCount; i++) {
-        if( indicators[i].checked ==  true) {
-            console.log('here')
-            setSlide(i)();
-            break;
+        for (let i = 0; i < indicatorsCount; i++) {
+            if( indicators[i].checked ==  true) {
+                console.log('here')
+                setSlide(Math.floor(i/2))();
+                break;
+            }
         }
     }
+
+    if (window.innerWidth <= 480 && itemsCount == 2) {
+        for (let i = 0; i < 6; i++) {
+            let node = document.createElement("input");
+
+            node.setAttribute("class", "indicator");
+            node.setAttribute("name", "indicator");
+            node.setAttribute("type", "radio");
+            node.setAttribute("data-slide", `${indicatorsCount + i}`);
+            node.addEventListener("click", setSlide(indicatorsCount + i));
+
+            indicatorsParent.appendChild(node);
+        }
+        itemsCount = 1;
+        indicatorsCount = Math.ceil(slides.length/itemsCount);
+
+        for (let i = 0; i < indicatorsCount; i++) {
+            if( indicators[i].checked ==  true) {
+                console.log('here');
+                setSlide(i*2)();
+                break;
+            }
+        }
+    }
+
+    if (window.innerWidth > 480 && itemsCount == 1) {
+
+        for (let i = 0; i < 6; i++) {
+            if(indicatorsParent.lastElementChild.checked == true) {
+              console.log("ok");
+              indicatorsParent.lastElementChild.previousElementSibling.checked = true;
+            }
+            indicatorsParent.removeChild(indicatorsParent.lastElementChild);
+        }
+        itemsCount = 2;
+        indicatorsCount = Math.ceil(slides.length/itemsCount);
+
+        for (let i = 0; i < indicatorsCount; i++) {
+            if( indicators[i].checked ==  true) {
+                console.log('here')
+                setSlide(Math.floor(i/2))();
+                break;
+            }
+        }
     }
 }
 
@@ -208,14 +146,14 @@ var itemsCount;
 var indicatorsCount;
 
 if(window.innerWidth > 800) {
-    itemsCount = 4; 
+    itemsCount = 4;
 } else if (window.innerWidth > 480) {
-    itemsCount = 2; 
+    itemsCount = 2;
 } else {
     itemsCount = 1;
 }
 
-indicatorsCount = Math.floor(slides.length/itemsCount);
+indicatorsCount = Math.ceil(slides.length/itemsCount);
 
 for (let i = 0; i < indicatorsCount; i++) {
     let node = document.createElement("input");
